d789d9cd2b13fe3e4dddd1c957e947ac4404496b
DeadIntegral <jegok@naver.com>
2016-02-18 14:18:13 +0900

    Create userData.js

---
+++ js/userData.js
@@ -0,0 +1,23 @@
+var userData = {
+	stat:{
+		pa:5,
+		hp:50,
+		exp:0
+	},
+	equip:{
+	},
+	unlock:{
+		stat:0,
+		statBtn:0,
+		menu:0,
+		design:0,
+		idle:0
+	},
+	field:{0:1},
+	hunt:{1:1,11:1,21:1},
+	killMonster:{},
+	killBoss:{},
+	status:{battle:0},
+	idle:{exp:0, gold:0}
+	
+};
