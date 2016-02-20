9387db71a113b01fe57e3294f7b59bf063bdfaae
DeadIntegral <jegok@naver.com>
2016-02-20 15:32:10 +0900

    Update userData.js

--- js/userData.js
+++ js/userData.js
@@ -8,14 +8,14 @@ var userData = {
 	},
 	unlock:{
 		stat:0,
+		eStat:0,
 		statBtn:0,
 		menu:0,
 		design:0,
 		idle:0
 	},
 	field:{0:1},
-	hunt:{1:1,11:1,21:1,31:1,41:1,51:1},
-	killMonster:{},
+	hunt:{},
 	killBoss:{},
 	status:{battle:0},
 	idle:{exp:0, gold:0}
