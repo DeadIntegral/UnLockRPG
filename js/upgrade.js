2bc22e28043adf6b0569512f14a5b0cb7ac790f9
DeadIntegral <jegok@naver.com>
2016-02-24 00:19:43 +0900

    Update upgrade.js

--- js/upgrade.js
+++ js/upgrade.js
@@ -4,7 +4,7 @@ var upgrade = {
 			0:{name:'Unlock Physical Defense', costExp:20, target:'pd', otherUnlock:'eStat'},
 			1:{name:'Unlock Magic Attack', costExp:170, target:'ma'},
 			2:{name:'Unlock Magic Defense', costExp:300, target:'md'},
-			3:{name:'Unlock Mana Point', costExp:5000, target:'mp', otherUnlock:'menu'},
+			3:{name:'Unlock Mana Point', costExp:500, target:'mp'},
 			4:{name:'Unlock Element Fire Attack', costExp:50000, target:'efa'},
 			5:{name:'Unlock Element Fire Defense', costExp:50000, target:'efd'},
 			6:{name:'Unlock Element Ice Attack', costExp:50000, target:'eia'},
@@ -20,19 +20,27 @@ var upgrade = {
 		statBtn:{
 			0:{name:'Unlock Up 1 Button', costExp:5},
 			1:{name:'Unlock Up 10 Button - shift+click', costExp:100},
-			2:{name:'Unlock Up 100 Button - alt+click', costExp:4000},
-			3:{name:'Unlock Up 1000 Button - ctrl+click', costExp:50000}
+			2:{name:'Unlock Up 100 Button - alt+click', costExp:2000},
+			3:{name:'Unlock Up 1000 Button - ctrl+click', costExp:40000}
 		},
 		idle:{
 			0:{name:'Unlock Generate +1 Exp/s', costExp:10, costGold:20, addExp:1},
-			1:{name:'Unlock Generate +1 Gold/s', costGold:50, addGold:1},
+			1:{name:'Unlock Generate +1 Gold/s', costGold:50, addGold:1, otherUnlock:'menu'},
 			2:{name:'Unlock Generate +2 Exp/s', costExp:100, costGold:500, addExp:2},
 			3:{name:'Unlock Generate +2 Gold/s', costGold:1000, addGold:2},
 		},
 		menu:{
 			0:{name:'Unlock Build Menu', costExp:100},
-			1:{name:'Unlock Skill Menu', costExp:1000}
+			1:{name:'Unlock Skill Menu', costExp:1000},
+			2:{name:'Unlock Bag Menu', costExp:20000},
+			3:{name:'Unlock Shop Menu', costExp:20000},
 		}
 	}
 	
 };
+var skill = {
+	active:{},
+	passive:{
+		0:{name:'', effect:'', costExp:10, maxLv:5}
+	}
+}
