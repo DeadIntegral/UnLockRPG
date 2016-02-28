82caa70f6a573d6a62b1038753fbba7bd7d1f9f3
DeadIntegral <jegok@naver.com>
2016-02-28 14:57:17 +0900

    Update skillData.js

--- js/skillData.js
+++ js/skillData.js
@@ -1,6 +1,50 @@
 var skill = {
 	active:{},
 	passive:{
-		1:{ name:'기초검술Ⅰ', add:'pa+20', costGold:100, need:1	}
+		1:{
+			1:{name:'Sword TrainingⅠ', add:'pa+40', costGold:100, costExp:100, need:1},
+			2:{name:'Sword TrainingⅡ', add:'pa+120', costGold:200, costExp:200, need:1},
+			3:{name:'Sword TrainingⅢ', add:'pa+240', costGold:300, costExp:300, need:1},
+			4:{name:'Sword TrainingⅣ', add:'pa+400', costGold:400, costExp:400, need:1},
+			5:{name:'Sword TrainingⅤ', add:'pa+600', costGold:500, costExp:500, need:1},
+		},
+		2:{
+			1:{name:'니토리검술Ⅰ', add:'pa+0', costGold:100, needSkill:1},
+			2:{name:'니토리검술Ⅱ', add:'pa+0', costGold:100, need:1},
+			3:{name:'니토리검술Ⅲ', add:'pa+0', costGold:100, need:1},
+		},
+		11:{
+			1:{name:'Guard TrainingⅠ', add:'pd+40', costGold:100, costExp:100, need:1 },
+			2:{name:'Guard TrainingⅡ', add:'pd+100', costGold:150, costExp:150, need:1 },
+			3:{name:'Guard TrainingⅢ', add:'pd+180', costGold:200, costExp:200, need:1 },
+			4:{name:'Guard TrainingⅣ', add:'pd+280', costGold:250, costExp:250, need:1 },
+			5:{name:'Guard TrainingⅤ', add:'pd+400', costGold:300, costExp:300, need:1 },
+		},
+		12:{
+			1:{name:'Amor MasteryⅠ', add:'pd+400', costGold:1000, costExp:1000, need:1 },
+			2:{name:'Amor MasteryⅡ', add:'pd+1000', costGold:1500, costExp:1500, need:1 },
+			3:{name:'Amor MasteryⅢ', add:'pd+1800', costGold:2000, costExp:2000, need:1 },
+			4:{name:'Amor MasteryⅣ', add:'pd+2800', costGold:2500, costExp:2500, need:1 },
+			5:{name:'Amor MasteryⅤ', add:'pd+4000', costGold:3000, costExp:3000, need:1 },
+		},
 	}
 }
+
+/*
+basic sword training
+트레이닝 -> (익스퍼트) -> 마스터
+
+active
+ 강타bash
+ 분노의 일격
+ magic sword
+ light sword
+ 
+ iron skin
+ Enchant
+ Hardening
+ 
+ 화염구fire ball
+ 
+ 쿨타임넣기 - 스턴기 등
+*/
