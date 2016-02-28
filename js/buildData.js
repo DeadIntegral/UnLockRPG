17d43c5a943634ddbf726068f609f6d0f88e9786
DeadIntegral <jegok@naver.com>
2016-02-28 14:58:51 +0900

    Update buildData.js

--- js/buildData.js
+++ js/buildData.js
@@ -2,11 +2,18 @@ var build = {
 	farm:{
 		0:{name:'Farm Lv1', costGold:1000, addGold:2},
 		1:{name:'Farm Lv2', costGold:5000, addGold:2},
+		2:{name:'Farm Lv3', costGold:10000, addGold:2},
 	},
 	cityH:{
 		0:{name:'City Hall Lv1', costGold:10000 }
 	},
 	trainH:{
-		0:{name:'Trainning Ground Lv1 - Allow train Skill', costGold:1000 }
+		0:{name:'Trainning Ground Lv1', costGold:1000, addExp:2 }
 	}
 }
+
+/*
+Farm
+City Hall
+Trainning Ground
+*/
