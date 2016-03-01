ab9b1dab8dbb87086bbdfaf1630208864daea6bc
DeadIntegral <jegok@naver.com>
2016-03-01 20:22:51 +0900

    Update buildData.js

--- js/buildData.js
+++ js/buildData.js
@@ -8,7 +8,9 @@ var build = {
 		0:{name:'City Hall Lv1', costGold:10000 }
 	},
 	trainH:{
-		0:{name:'Trainning Ground Lv1', costGold:1000, addExp:2 }
+		0:{name:'Trainning Ground Lv1', costGold:1000, addExp:2 },
+		1:{name:'Trainning Ground Lv2', costGold:5000, addExp:2 },
+		2:{name:'Trainning Ground Lv3', costGold:10000, addExp:2 }
 	}
 }
 
